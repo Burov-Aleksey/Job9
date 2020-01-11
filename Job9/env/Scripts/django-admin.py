c62a6958d145e606f6d30f6418ccb8a8bdcc569d
Burov-Aleksey <Burov.alexey.mihaylovich@gmail.com>
2020-01-11 22:05:29 +0300

    Добавьте файлы проекта.

---
+++ Job9/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\178365\Desktop\Веб программирование\Job9\Job9\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
